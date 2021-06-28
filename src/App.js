6fb9223ae751fb867c773dcf13bba859b009fdbe
Dheeraj Sharma <dhiru3599@gmail.com>
2021-06-29 00:22:10 +0530

    feat: add header to app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <div className="App__header">
+        <img
+          className="App__headerImage"
+          src="https://instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
       <h1>react-instagram-clone</h1>
     </div>
   );
