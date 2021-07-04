e5b2362d0b2984e90e6e89290a2e7dc345ae043b
Dheeraj Sharma <dhiru3599@gmail.com>
2021-07-04 17:31:16 +0530

    feat: add avatar to post header

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,17 +1,25 @@
 import React from "react";
 import "../styles/Post.css";
+import Avatar from "@material-ui/core/Avatar";
 
 function Post() {
   return (
     <div className="post">
-      <h3>Username</h3>
-      {/*avatar*/}
+      <div className="post__header">
+        <Avatar
+          className="post__avatar"
+          alt="avatar"
+          src="/static/images/avatar.1.jpg"
+        />
+        <h3>Username</h3>
+      </div>
+
       <img
         className="post__image"
         src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
       />
-      <p>
-        <strong>Username: </strong> caption for the post
+      <p className="post__text">
+        <strong>Username </strong> caption for the post
       </p>
     </div>
   );
